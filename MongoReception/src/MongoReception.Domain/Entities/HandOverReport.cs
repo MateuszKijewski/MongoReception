f159f003f97f23361f1cae2f22bde2c43dc199c2
Mateusz <mateuszkijewski2307@gmail.com>
2021-06-19 05:49:17 +0200

    Added handoverreports

---
+++ MongoReception/src/MongoReception.Domain/Entities/HandOverReport.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoReception.Domain.Common.Interfaces;
+
+namespace MongoReception.Domain.Entities
+{
+    public class HandOverReport : IMongoEntity
+    {
+        [BsonId]
+        public string Id { get; set; }
+
+        [BsonRepresentation(BsonType.String)]
+        public string Name { get; set; }
+
+        [BsonRepresentation(BsonType.String)]
+        public string Description { get; set; }
+
+        [BsonRepresentation(BsonType.String)]
+        public string ReservationId { get; set; }
+
+        public BsonArray Details { get; set; }
+
+        public string GetCollectionName()
+        {
+            return "HandOverReports";
+        }
+    }
+}
---
+++ MongoReception/src/MongoReception.Infrastructure/Common/Interfaces/IHandOverReportRepository.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoReception.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace MongoReception.Infrastructure.Common.Interfaces
+{
+    public interface IHandOverReportRepository : IBaseRepository<HandOverReport>
+    {
+        Task AttachDetails(string handOverReportId, BsonArray details);
+
+        Task AddWithDetails(HandOverReport handOverReport, BsonArray details);
+    }
+}
